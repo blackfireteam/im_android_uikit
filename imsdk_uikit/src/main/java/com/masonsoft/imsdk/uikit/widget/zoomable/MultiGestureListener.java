e6513a3154f545a351de5f58816d3b177d342f88
idonans <idonans@126.com>
2021-11-09 11:18:38 +0800

    sync widget MultiGestureListener synchronized

--- imsdk_uikit/src/main/java/com/masonsoft/imsdk/uikit/widget/zoomable/MultiGestureListener.java
+++ imsdk_uikit/src/main/java/com/masonsoft/imsdk/uikit/widget/zoomable/MultiGestureListener.java
@@ -140,7 +140,7 @@ public class MultiGestureListener extends GestureDetector.SimpleOnGestureListene
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
-    public boolean onContextClick(MotionEvent e) {
+    public synchronized boolean onContextClick(MotionEvent e) {
         final int size = mListeners.size();
         for (int i = 0; i < size; i++) {
             if (mListeners.get(i).onContextClick(e)) {
