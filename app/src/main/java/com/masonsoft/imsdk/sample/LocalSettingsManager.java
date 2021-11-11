6190047a888676086d88d16ecc3969b903c46965
idonans <idonans@126.com>
2021-11-11 11:00:51 +0800

    fix ref error

--- app/src/main/java/com/masonsoft/imsdk/sample/LocalSettingsManager.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/LocalSettingsManager.java
@@ -10,6 +10,9 @@ import com.google.gson.reflect.TypeToken;
 import com.masonsoft.imsdk.MSIMManager;
 import com.masonsoft.imsdk.util.Objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.github.idonans.core.Singleton;
 import io.github.idonans.core.manager.StorageManager;
 import io.github.idonans.core.thread.Threads;
@@ -31,9 +34,12 @@ public class LocalSettingsManager {
 
     @NonNull
     private Settings mSettings = new Settings();
+    @NonNull
+    private final ApiServerLru mApiServerLru = new ApiServerLru();
 
     private LocalSettingsManager() {
         restore();
+        mApiServerLru.restore();
     }
 
     public void start() {
@@ -104,6 +110,91 @@ public class LocalSettingsManager {
             target.imServer = this.imServer;
             return target;
         }
+    }
+
+    @NonNull
+    public ApiServerLru getApiServerLru() {
+        return mApiServerLru;
+    }
+
+    /**
+     * 记录用户输入的 api server 历史记录，lru
+     */
+    public static class ApiServerLru {
+        private static final String KEY_API_SERVER_LRU = "key:ApiServerLru_20211102";
+        private static final int MAX_SIZE = 5;
+        private final List<String> mCache = new ArrayList<>();
+
+        private ApiServerLru() {
+            // 添加默认值
+            mCache.add("https://im.ekfree.com:18788");
+            mCache.add("https://192.168.50.189:18788");
+        }
+
+        public void addApiServer(@Nullable String apiServer) {
+            this.addApiServer(apiServer, false, true);
+        }
+
+        private void addApiServer(@Nullable String apiServer, boolean append, boolean save) {
+            boolean changed = false;
+            if (!TextUtils.isEmpty(apiServer)) {
+                //noinspection ConstantConditions
+                apiServer = apiServer.trim();
+                synchronized (mCache) {
+                    mCache.remove(apiServer);
+                    if (append) {
+                        mCache.add(apiServer);
+                    } else {
+                        mCache.add(0, apiServer);
+                    }
+
+                    if (mCache.size() > MAX_SIZE) {
+                        mCache.remove(mCache.size() - 1);
+                    }
+                }
+                changed = true;
+            }
+            if (changed && save) {
+                saveAsync();
+            }
+        }
+
+        @NonNull
+        public final List<String> allApiServer() {
+            synchronized (mCache) {
+                return new ArrayList<>(mCache);
+            }
+        }
+
+        private void restore() {
+            try {
+                final String json = StorageManager.getInstance().get(Constants.SAMPLE_STORAGE_NAMESPACE, KEY_API_SERVER_LRU);
+                if (!TextUtils.isEmpty(json)) {
+                    final List<String> apiServerList = new Gson().fromJson(json, new TypeToken<List<String>>() {
+                    }.getType());
+                    if (apiServerList != null) {
+                        for (String apiServer : apiServerList) {
+                            addApiServer(apiServer, true, false);
+                        }
+                    }
+                }
+            } catch (Throwable e) {
+                SampleLog.e(e);
+            }
+        }
+
+        private void saveAsync() {
+            Threads.postBackground(() -> {
+                try {
+                    final List<String> cache = allApiServer();
+                    final String json = new Gson().toJson(cache);
+                    StorageManager.getInstance().set(Constants.SAMPLE_STORAGE_NAMESPACE, KEY_API_SERVER_LRU, json);
+                } catch (Throwable e) {
+                    SampleLog.e(e);
+                }
+            });
+        }
+
     }
 
 }
--- app/src/main/java/com/masonsoft/imsdk/sample/uniontype/SampleUnionTypeMapper.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/uniontype/SampleUnionTypeMapper.java
@@ -2,6 +2,7 @@ package com.masonsoft.imsdk.sample.uniontype;
 
 import com.masonsoft.imsdk.sample.uniontype.viewholder.DiscoverUserViewHolder;
 import com.masonsoft.imsdk.sample.uniontype.viewholder.HomeSparkViewHolder;
+import com.masonsoft.imsdk.sample.uniontype.viewholder.SimpleTextViewHolder;
 
 import io.github.idonans.dynamic.uniontype.loadingstatus.UnionTypeLoadingStatus;
 
@@ -11,10 +12,12 @@ public class SampleUnionTypeMapper extends UnionTypeLoadingStatus {
 
     public static final int UNION_TYPE_IMPL_IM_HOME_SPARK = sNextUnionType++; // 首页中的一条 spark
     public static final int UNION_TYPE_IMPL_IM_DISCOVER_USER = sNextUnionType++; // 发现页中的一个 user 信息
+    public static final int UNION_TYPE_IMPL_IM_SIMPLE_TEXT = sNextUnionType++; // 简单文本显示样式，例如用于登录页选择最近使用的地址
 
     public SampleUnionTypeMapper() {
         put(UNION_TYPE_IMPL_IM_HOME_SPARK, HomeSparkViewHolder::new);
         put(UNION_TYPE_IMPL_IM_DISCOVER_USER, DiscoverUserViewHolder::new);
+        put(UNION_TYPE_IMPL_IM_SIMPLE_TEXT, SimpleTextViewHolder::new);
     }
 
 }
