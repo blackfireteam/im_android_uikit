9f505b690b69cd9c5560e7292b263126a1233160
idonans <idonans@126.com>
2021-11-17 10:49:54 +0800

    add SingleChatActivity check targetUserId

--- imsdk_uikit/src/main/java/com/masonsoft/imsdk/uikit/app/chat/SingleChatActivity.java
+++ imsdk_uikit/src/main/java/com/masonsoft/imsdk/uikit/app/chat/SingleChatActivity.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 
 import com.masonsoft.imsdk.uikit.MSIMUikitConstants;
 import com.masonsoft.imsdk.uikit.app.FragmentDelegateActivity;
+import com.masonsoft.imsdk.uikit.util.TipUtil;
 
 import io.github.idonans.systeminsets.SystemUiHelper;
 
@@ -35,6 +36,11 @@ public class SingleChatActivity extends FragmentDelegateActivity {
                 .apply();
 
         final long targetUserId = getIntent().getLongExtra(MSIMUikitConstants.ExtrasKey.TARGET_USER_ID, 0L);
+        if (targetUserId <= 0) {
+            TipUtil.show(MSIMUikitConstants.ErrorLog.INVALID_USER_ID);
+            finish();
+            return;
+        }
         setFragmentDelegate(FRAGMENT_TAG_SINGLE_CHAT, () -> SingleChatFragment.newInstance(targetUserId));
     }
 
