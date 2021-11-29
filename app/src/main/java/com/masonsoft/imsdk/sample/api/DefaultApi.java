eaf746bcd12dc9c7d8f6a11973c7c70444ceda56
idonans <idonans@126.com>
2021-11-29 16:20:27 +0800

    add master app3 config

--- app/src/main/java/com/masonsoft/imsdk/sample/api/DefaultApi.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/api/DefaultApi.java
@@ -79,7 +79,7 @@ public class DefaultApi {
                 .addHeader("nonce", String.valueOf(nonce))
                 .addHeader("timestamp", String.valueOf(timestamp))
                 .addHeader("sig", sign)
-                .addHeader("app_id", "2")
+                .addHeader("app_id", "3")
                 .url(url)
                 .post(requestBody)
                 .build();
