e8168d855f1b256bf788e3d58d8dd7b17da89cef
idonans <idonans@126.com>
2021-11-30 15:32:08 +0800

    add app_id 1

--- app/src/main/java/com/masonsoft/imsdk/sample/api/DefaultApi.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/api/DefaultApi.java
@@ -79,6 +79,7 @@ public class DefaultApi {
                 .addHeader("nonce", String.valueOf(nonce))
                 .addHeader("timestamp", String.valueOf(timestamp))
                 .addHeader("sig", sign)
+                .addHeader("app_id", "1")
                 .url(url)
                 .post(requestBody)
                 .build();
