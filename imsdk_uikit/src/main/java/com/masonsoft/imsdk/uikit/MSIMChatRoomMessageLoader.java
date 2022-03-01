e1850903790d8699e1797228b506f1ac95b761df
idonans <idonans@126.com>
2022-03-01 18:00:09 +0800

    tmp save ...4

---
+++ imsdk_uikit/src/main/java/com/masonsoft/imsdk/uikit/MSIMChatRoomMessageLoader.java
@@ -0,0 +1,96 @@
+package com.masonsoft.imsdk.uikit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.masonsoft.imsdk.MSIMChatRoomContext;
+import com.masonsoft.imsdk.MSIMChatRoomMessage;
+import com.masonsoft.imsdk.util.Objects;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import io.github.idonans.core.util.IOUtil;
+
+public abstract class MSIMChatRoomMessageLoader extends DataLoaderImpl<MSIMChatRoomMessage> implements Closeable {
+
+    private static final boolean DEBUG = MSIMUikitConstants.DEBUG_WIDGET;
+
+    @NonNull
+    private final MSIMChatRoomContext mChatRoomContext;
+    private final MSIMChatRoomMessageChangedHelper mHelper;
+
+    @Nullable
+    protected MSIMChatRoomMessage mMessage;
+
+    public MSIMChatRoomMessageLoader(@NonNull MSIMChatRoomContext chatRoomContext) {
+        mChatRoomContext = chatRoomContext;
+        mHelper = new MSIMChatRoomMessageChangedHelper(chatRoomContext) {
+            @Override
+            protected void onMessageChanged(@NonNull MSIMChatRoomMessage message) {
+                super.onMessageChanged(message);
+
+                MSIMChatRoomMessageLoader.this.onMessageChangedInternal(message, false);
+            }
+        };
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+        IOUtil.closeQuietly(mHelper);
+    }
+
+    @Nullable
+    public MSIMChatRoomMessage getMessage() {
+        return mMessage;
+    }
+
+    public void setMessage(@Nullable MSIMChatRoomMessage message) {
+        setMessageInternal(message);
+    }
+
+    private void setMessageInternal(@Nullable MSIMChatRoomMessage message) {
+        mMessage = message;
+        onMessageLoad(mMessage);
+    }
+
+    private void onMessageChangedInternal(@Nullable MSIMChatRoomMessage message, boolean acceptNull) {
+        if (!acceptNull && message == null) {
+            return;
+        }
+
+        if (message != null && mMessage != null && !message.equals(mMessage)) {
+            return;
+        }
+
+        mMessage = message;
+        onMessageLoad(mMessage);
+    }
+
+    protected void onMessageLoad(@Nullable MSIMChatRoomMessage message) {
+        if (DEBUG) {
+            MSIMUikitLog.v("%s onMessageLoad %s", Objects.defaultObjectTag(this), message);
+        }
+    }
+
+    @Override
+    public void requestLoadData() {
+        MSIMUikitLog.v("%s do not require requestLoadData", Objects.defaultObjectTag(this));
+    }
+
+    @Nullable
+    @Override
+    protected MSIMChatRoomMessage loadData() {
+        MSIMUikitLog.v("%s not impl loadData", Objects.defaultObjectTag(this));
+        // ignore
+        return null;
+    }
+
+    @Override
+    protected void onDataLoad(@Nullable MSIMChatRoomMessage data) {
+        MSIMUikitLog.v("%s not impl onDataLoad", Objects.defaultObjectTag(this));
+        // ignore
+    }
+
+}
---
+++ imsdk_uikit/src/main/java/com/masonsoft/imsdk/uikit/MSIMMessageLoader.java
@@ -0,0 +1,97 @@
+package com.masonsoft.imsdk.uikit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.masonsoft.imsdk.MSIMManager;
+import com.masonsoft.imsdk.MSIMMessage;
+import com.masonsoft.imsdk.util.Objects;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import io.github.idonans.core.util.IOUtil;
+
+public abstract class MSIMMessageLoader extends DataLoaderImpl<MSIMMessage> implements Closeable {
+
+    private static final boolean DEBUG = MSIMUikitConstants.DEBUG_WIDGET;
+
+    private final MSIMMessageChangedHelper mHelper;
+
+    @Nullable
+    protected MSIMMessage mMessage;
+
+    public MSIMMessageLoader() {
+        mHelper = new MSIMMessageChangedHelper() {
+            @Override
+            protected void onMessageChanged(@NonNull MSIMMessage message) {
+                super.onMessageChanged(message);
+
+                MSIMMessageLoader.this.onMessageChangedInternal(message, false);
+            }
+        };
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+        IOUtil.closeQuietly(mHelper);
+    }
+
+    @Nullable
+    public MSIMMessage getMessage() {
+        return mMessage;
+    }
+
+    public void setMessage(@Nullable MSIMMessage message) {
+        setMessageInternal(message);
+    }
+
+    private void setMessageInternal(@Nullable MSIMMessage message) {
+        mMessage = message;
+        onMessageLoad(mMessage);
+
+        requestLoadData();
+    }
+
+    private void onMessageChangedInternal(@Nullable MSIMMessage message, boolean acceptNull) {
+        if (!acceptNull && message == null) {
+            return;
+        }
+
+        if (message != null && mMessage != null && !message.equals(mMessage)) {
+            return;
+        }
+
+        mMessage = message;
+        onMessageLoad(mMessage);
+    }
+
+    protected void onMessageLoad(@Nullable MSIMMessage message) {
+        if (DEBUG) {
+            MSIMUikitLog.v("%s onMessageLoad %s", Objects.defaultObjectTag(this), message);
+        }
+    }
+
+    @Nullable
+    @Override
+    protected MSIMMessage loadData() {
+        final MSIMMessage message = mMessage;
+        if (message == null) {
+            return null;
+        }
+
+        return MSIMManager.getInstance().getMessageManager().getMessage(
+                message.getSessionUserId(),
+                message.getConversationType(),
+                message.getTargetUserId(),
+                message.getMessageId()
+        );
+    }
+
+    @Override
+    protected void onDataLoad(@Nullable MSIMMessage message) {
+        onMessageChangedInternal(message, true);
+    }
+
+}
