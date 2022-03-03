d32e96e887cca678f17068dda55f4dae343b9abd
idonans <idonans@126.com>
2022-03-03 14:42:08 +0800

    add HomeSparkViewHolder support auto user info load

--- app/src/main/java/com/masonsoft/imsdk/sample/uniontype/viewholder/HomeSparkViewHolder.java
+++ app/src/main/java/com/masonsoft/imsdk/sample/uniontype/viewholder/HomeSparkViewHolder.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 
 import androidx.annotation.FloatRange;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.masonsoft.imsdk.MSIMManager;
 import com.masonsoft.imsdk.MSIMMessage;
+import com.masonsoft.imsdk.MSIMUserInfo;
 import com.masonsoft.imsdk.sample.R;
 import com.masonsoft.imsdk.sample.SampleLog;
 import com.masonsoft.imsdk.sample.databinding.ImsdkSampleUnionTypeImplHomeSparkBinding;
@@ -15,6 +17,7 @@ import com.masonsoft.imsdk.sample.util.JsonUtil;
 import com.masonsoft.imsdk.sample.util.StringUtil;
 import com.masonsoft.imsdk.uikit.CustomIMMessageFactory;
 import com.masonsoft.imsdk.uikit.MSIMUikitConstants;
+import com.masonsoft.imsdk.uikit.MSIMUserInfoLoader;
 import com.masonsoft.imsdk.uikit.app.chat.SingleChatActivity;
 import com.masonsoft.imsdk.uikit.uniontype.DataObject;
 import com.masonsoft.imsdk.util.Objects;
@@ -32,10 +35,20 @@ import io.github.idonans.uniontype.UnionTypeViewHolder;
 public class HomeSparkViewHolder extends UnionTypeViewHolder {
 
     private final ImsdkSampleUnionTypeImplHomeSparkBinding mBinding;
+    private final MSIMUserInfoLoader mUserInfoLoader;
 
     public HomeSparkViewHolder(@NonNull Host host) {
         super(host, R.layout.imsdk_sample_union_type_impl_home_spark);
         mBinding = ImsdkSampleUnionTypeImplHomeSparkBinding.bind(itemView);
+
+        mUserInfoLoader = new MSIMUserInfoLoader() {
+            @Override
+            protected void onUserInfoLoad(long userId, @Nullable MSIMUserInfo userInfo) {
+                super.onUserInfoLoad(userId, userInfo);
+
+                HomeSparkViewHolder.this.onUserInfoLoad(userId, userInfo);
+            }
+        };
     }
 
     public void updateLikeAndDislike(@FloatRange(from = -1, to = 1) float progress) {
@@ -65,15 +78,36 @@ public class HomeSparkViewHolder extends UnionTypeViewHolder {
         }
     }
 
+    private void onUserInfoLoad(long userId, @Nullable MSIMUserInfo userInfo) {
+        final DataObject itemObject = getItemObject(DataObject.class);
+        if (itemObject == null) {
+            return;
+        }
+
+        final Spark spark = itemObject.getObject(Spark.class);
+        if (spark == null) {
+            return;
+        }
+
+        if (spark.profile.getUid() != userId) {
+            return;
+        }
+
+        mBinding.username.setUserInfo(userId, userInfo);
+    }
+
     @Override
     public void onBindUpdate() {
         final DataObject itemObject = getItemObject(DataObject.class);
         Preconditions.checkNotNull(itemObject);
         final Spark spark = (Spark) itemObject.object;
+        Preconditions.checkNotNull(spark);
+
+        mUserInfoLoader.setUserInfo(spark.profile.getUid(), null);
 
         mBinding.imageLayout.setImageUrl(null, spark.profile.getAvatar());
-        mBinding.username.setUserInfo(spark.profile.getUid(), null);
         mBinding.desc.setText(buildDescText(spark));
+
         updateLikeAndDislike(0, false);
 
         final ExtraUiData extraUiData = ExtraUiData.valueOf(itemObject);
